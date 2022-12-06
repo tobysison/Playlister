b35f96f8078cf176b96e1d0a2aa796ebeedf8f74
tobysison <90851370+tobysison@users.noreply.github.com>
2022-12-06 01:12:43 -0500

    +EditToolbar
    Changes to EditToolbar.js

--- Playlister Project/client/src/components/EditToolbar.js
+++ Playlister Project/client/src/components/EditToolbar.js
@@ -4,14 +4,8 @@ import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
 import RedoIcon from '@mui/icons-material/Redo';
 import UndoIcon from '@mui/icons-material/Undo';
-import CloseIcon from '@mui/icons-material/HighlightOff';
+import { Box } from '@mui/material';
 
-/*
-    This toolbar is a functional React component that
-    manages the undo/redo/close buttons.
-    
-    @author McKilla Gorilla
-*/
 function EditToolbar() {
     const { store } = useContext(GlobalStoreContext);
 
@@ -24,40 +18,19 @@ function EditToolbar() {
     function handleRedo() {
         store.redo();
     }
-    function handleClose() {
-        store.closeCurrentList();
-    }
+
     return (
-        <div id="edit-toolbar">
-            <Button
-                disabled={!store.canAddNewSong() || store.currentModal !== 'NONE'}
-                id='add-song-button'
-                onClick={handleAddNewSong}
-                variant="contained">
-                <AddIcon />
+        <Box id="edit-toolbar">
+            <Button disabled={!store.canAddNewSong()} id='add-song-button' onClick={handleAddNewSong} variant="contained">
+                <AddIcon/> Add
             </Button>
-            <Button 
-                disabled={!store.canUndo() || store.currentModal !== 'NONE'}
-                id='undo-button'
-                onClick={handleUndo}
-                variant="contained">
-                    <UndoIcon />
+            <Button disabled={!store.canUndo()} id='undo-button' onClick={handleUndo} variant="contained">
+                <UndoIcon/> Undo
             </Button>
-            <Button 
-                disabled={!store.canRedo() || store.currentModal !== 'NONE'}
-                id='redo-button'
-                onClick={handleRedo}
-                variant="contained">
-                    <RedoIcon />
+            <Button disabled={!store.canRedo()} id='redo-button' onClick={handleRedo} variant="contained">
+                <RedoIcon/> Redo
             </Button>
-            <Button 
-                disabled={!store.canClose() || store.currentModal !== 'NONE'}
-                id='close-button'
-                onClick={handleClose}
-                variant="contained">
-                    <CloseIcon />
-            </Button>
-        </div>
+        </Box>
     )
 }
 
